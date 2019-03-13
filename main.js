30fc170b1446abaade6e02c312776c04d38914f1
Thomas Weiß <thomas0weiss@googlemail.com>
2019-03-13 10:25:26 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,18 +1,49 @@
+const Tesseract = require('tesseract.js')
 const vision = require('@google-cloud/vision');
- 
 const client = new vision.ImageAnnotatorClient({
-      keyFilename: './key.json',  //Service account keyfile
-      projectId: 'project_id' //Your project ID from Google Cloud
+      keyFilename: './key.json',
+      projectId: 'reader-234408'
 });
 
-var timer = Date.now();
- 
-client
-  .textDetection('./tester.jpg') //Detect this image's text
-  .then(results => {
-    console.log(results[0].fullTextAnnotation.text);
-    console.log("Took " + (Date.now() - timer) + "ms")
-  })
-  .catch(err => {
-    console.error('ERROR:', err);
-  });
+runOCR("./tester.jpg")
+
+function runOCR(image){
+    require('dns').resolve('www.google.com', function(err) {
+      if (err) {
+      	 console.log("\n===============================================================\n > No data connection, using tesseract");
+      	 tesseractOCR(image)
+      } else {
+      	 console.log("\n===============================================================\n > Data connection available, using GCloud");
+      	 gcloudOCR(image)
+      }
+    });	
+}
+
+
+function gcloudOCR(image){
+    var timer = Date.now();
+    client
+    	.textDetection(image)
+    	.then(results => {
+    	  outOCR(results[0].fullTextAnnotation.text, timer, "Google Cloud OCR")
+    	})
+    	.catch(err => {
+    	  console.error('ERROR:', err);
+    	});	
+}
+
+function tesseractOCR(image){
+	var timer = Date.now();
+    Tesseract.recognize(image)
+       .progress(function(result){
+       	//console.log(result)
+       })
+       .then(function (result) { 
+       		outOCR(result.text, timer, "TesseractJS")
+       	})	
+}
+
+function outOCR(text, time, service){
+	var finished = (Date.now() - time) / 1000;
+	console.log("===============================================================\n" + text + "===============================================================\n > Service: " + service + "\n > Time: " + finished.toFixed(1) + " seconds\n===============================================================\n\n")
+}
